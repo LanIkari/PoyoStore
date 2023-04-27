ffa121576f8eba7c0cf44787932778ec77a48e9c
Lan Ikari <angel_bazques@outlook.com>
2023-04-26 21:34:13 -0600

    Correcion de ruta index; Ya es posible acceder al front

--- src/main/java/com/proyecto/poyostore/controller/FacturaController.java
+++ src/main/java/com/proyecto/poyostore/controller/FacturaController.java
@@ -5,10 +5,7 @@ import com.proyecto.poyostore.service.FacturaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
--- src/main/java/com/proyecto/poyostore/controller/ProveedorController.java
+++ src/main/java/com/proyecto/poyostore/controller/ProveedorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("proveedores")
+@RequestMapping("/proveedores")
 public class ProveedorController {
     // Inyectamos la interfaz de usuario en una variable de tipo privada con @Autowired
     @Autowired
